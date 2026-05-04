f924680d01f92991a0c335df93180189a57cf324
alexboyce3 <alexboyce3@gmail.com>
2026-05-03 19:39:21 -0600

    page naming

--- streamlit_utils.py
+++ streamlit_utils.py
@@ -3,4 +3,4 @@ import streamlit as st
 def Navbar():
     with st.sidebar:
         st.page_link('standings_line.py', label='Standings and Streaks', icon='📈')
-        st.page_link('pages/page2.py', label='Team Scoring', icon='⚾')
+        st.page_link('pages/team_stats.py', label='Team Scoring', icon='⚾')
